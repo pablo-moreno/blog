4c5247b1523157aa55aa129391a6a344d6d27751
Pablo Moreno <pablo@kasfactory.net>
2022-02-21 17:44:00 +0100

    chore: changed title

--- components/AppWelcome.vue
+++ components/AppWelcome.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="w-full flex m-3">
     <header class="w-full flex items-center align-center flex-col">
-      <h2 class="text-5xl">Bienvenidos</h2>
+      <h2 class="text-5xl">Hola</h2>
       <img class="max-w-sm" src="itsme.png" alt="pablo" />
     </header>
   </div>
